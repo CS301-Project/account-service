d8d99abaf950f5276cbf23af09d7837351ca36ec
lynnkwl <143063371+lynnkwl@users.noreply.github.com>
2025-10-06 16:16:11 +0800

    #6: set up ci pipeline

--- src/main/java/com/bank/crm/accountservice/AccountServiceApplication.java
+++ src/main/java/com/bank/crm/accountservice/AccountServiceApplication.java
@@ -1,4 +1,4 @@
-package com.bank.crm.account_service;
+package com.bank.crm.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
