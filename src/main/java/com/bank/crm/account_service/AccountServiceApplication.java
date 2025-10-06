eb1fcf627a4b28ece073aa9361d9629d8f606ff4
adriank0h <adrian.koh.2022@scis.smu.edu.sg>
2025-10-06 17:57:55 +0800

    fix: File structure error

--- src/main/java/com/bank/crm/account_service/AccountServiceApplication.java
+++ src/main/java/com/bank/crm/account_service/AccountServiceApplication.java
@@ -1,4 +1,4 @@
-package com.bank.crm.accountservice;
+package com.bank.crm.account_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
