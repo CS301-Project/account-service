349eeb034a23b947cde6bb226f9a9374e97000dd
lynnkwl <143063371+lynnkwl@users.noreply.github.com>
2025-10-06 16:18:21 +0800

    #6: fix linting errors

--- src/main/java/com/bank/crm/accountservice/AccountServiceApplication.java
+++ src/main/java/com/bank/crm/accountservice/AccountServiceApplication.java
@@ -3,11 +3,14 @@ package com.bank.crm.accountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class for the Account Service.
+ */
 @SpringBootApplication
 public class AccountServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AccountServiceApplication.class, args);
+  }
 
 }
