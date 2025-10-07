454fceb21079f2e1c307e7625bcf9b73fe2ef4dc
lynnkwl <143063371+lynnkwl@users.noreply.github.com>
2025-10-08 03:03:32 +0800

    refactor: use lombok
    - use lombok
    - write tests for the account service and controller

--- src/main/java/com/bank/crm/account_service/dto/AccountResponse.java
+++ src/main/java/com/bank/crm/account_service/dto/AccountResponse.java
@@ -5,7 +5,13 @@ import com.bank.crm.account_service.model.AccountStatus;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountResponse {
 
     private UUID id;
@@ -16,85 +22,4 @@ public class AccountResponse {
     private BigDecimal initialDeposit;
     private String currency;
     private Integer branchId;
-
-    // Constructors
-    public AccountResponse() {}
-
-    public AccountResponse(UUID id, UUID clientId, AccountType accType, AccountStatus accStatus,
-                          LocalDateTime openingDate, BigDecimal initialDeposit,
-                          String currency, Integer branchId) {
-        this.id = id;
-        this.clientId = clientId;
-        this.accType = accType;
-        this.accStatus = accStatus;
-        this.openingDate = openingDate;
-        this.initialDeposit = initialDeposit;
-        this.currency = currency;
-        this.branchId = branchId;
-    }
-
-    // Getters and Setters
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public UUID getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(UUID clientId) {
-        this.clientId = clientId;
-    }
-
-    public AccountType getAccType() {
-        return accType;
-    }
-
-    public void setAccType(AccountType accType) {
-        this.accType = accType;
-    }
-
-    public AccountStatus getAccStatus() {
-        return accStatus;
-    }
-
-    public void setAccStatus(AccountStatus accStatus) {
-        this.accStatus = accStatus;
-    }
-
-    public LocalDateTime getOpeningDate() {
-        return openingDate;
-    }
-
-    public void setOpeningDate(LocalDateTime openingDate) {
-        this.openingDate = openingDate;
-    }
-
-    public BigDecimal getInitialDeposit() {
-        return initialDeposit;
-    }
-
-    public void setInitialDeposit(BigDecimal initialDeposit) {
-        this.initialDeposit = initialDeposit;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public Integer getBranchId() {
-        return branchId;
-    }
-
-    public void setBranchId(Integer branchId) {
-        this.branchId = branchId;
-    }
 }
