bc1639896f953b085b05e34460a37bfdae353f67
Ryan <ryanang333@gmail.com>
2025-10-29 17:37:15 +0800

    #27: expose /health endpoint for alb

--- src/main/java/com/bank/crm/account_service/config/SecurityConfig.java
+++ src/main/java/com/bank/crm/account_service/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/api/**"
+                                "/actuator/health"
                         ).permitAll()
                         .anyRequest().permitAll()
                 );
